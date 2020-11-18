f5c3f5157a7ea04e0016c1c75aec3b0b88659376
riwersxd <guilherme.rc.98@gmail.com>
2020-11-18 02:19:23 -0300

    Ordena retorno do historico

--- src/modules/historic/controller/historic.controller.ts
+++ src/modules/historic/controller/historic.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { HistoricService } from '../services/historicService';
 import { ChildService } from '../../child/services/childService';
+import { func } from '@hapi/joi';
 
 export default class HistoricController{
     public async postHistoric(request: Request, response: Response): Promise<Response> {
@@ -18,11 +19,22 @@ export default class HistoricController{
 
         const historicService = new HistoricService();
         const historic = await historicService.getHistoric({ childId })
+        historic.sort((a, b) => Number(a.measurement_date) - Number(b.measurement_date))
 
         const childService = new ChildService();
         const child = await childService.getChildById({ id_child: childId })
 
+        const state = {
+            height: historic[historic.length - 1].height,
+            weight: historic[historic.length - 1].weight,
+            age: (Date.now() - +new Date(child ? child.birthday : '')) / 31557600000,
+            status: ''
+        }
 
-        return response.status(200).json({ ...child, historic });
+        return response.status(200).json({
+            ...child,
+            ...state,
+            historic
+        });
     }
 }
