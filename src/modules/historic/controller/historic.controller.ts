a68660c919ebfca74bd0c462cde49f4e33d6af6f
riwersxd <guilherme.rc.98@gmail.com>
2020-12-03 21:14:25 -0300

    AGORA VAI KCTA

--- src/modules/historic/controller/historic.controller.ts
+++ src/modules/historic/controller/historic.controller.ts
@@ -4,7 +4,38 @@ import { ChildService } from '../../child/services/childService';
 import { ClassificationService } from '../../classification/services/classificationService'
 
 function calcAge (time = new Date) {
-    return ~~(((Date.now() - +new Date(time)) / 31557600000) * 12)
+    const nowDate = new Date()
+    const yearNow = nowDate.getFullYear() 
+    const monthNow = nowDate.getMonth() + 1
+    const dateNow = nowDate.getDate()
+
+    const timeDate = new Date(time)
+    const yearTime = timeDate.getFullYear()
+    const monthTime = timeDate.getMonth() + 1
+    const dateTime = timeDate.getDate()
+
+    let yearAge = yearNow - yearTime
+    let monthAge: number, dateAge: number
+    if (monthNow >= monthTime) {
+        monthAge = monthNow - monthNow
+    } else {
+        yearAge--
+        monthAge = 12 + monthNow - monthNow
+    }
+
+    if (dateNow >= dateTime) {
+        dateAge = dateNow - dateTime
+    } else {
+        monthAge--
+        dateAge = 31 + dateNow - dateTime
+        if (monthAge < 0) {
+            monthAge = 11
+            yearAge--
+        }
+    }
+
+    return (yearAge * 12) + monthAge
+    // return ~~(((Date.now() - +new Date(time)) / 31557600000) * 12)
 }
 
 function getStatus (classifications: any[], imc: number) {
